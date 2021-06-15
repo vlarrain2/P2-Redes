5f6b58c2a86e1f8db37da3ed0b9871a113e5cb32
Mateo <maormeno@uc.cl>
2021-06-15 12:57:48 -0400

    copy + clases

--- src/monster_hunter/classes.c
+++ src/monster_hunter/classes.c
@@ -45,10 +45,137 @@ Clase* clase_init(int type)
         clase -> initial_health = 25000;
         clase -> current_health = clase -> initial_health;
     }
+    introduce_player(type);
 
     return clase;
 }
 
+
+int introduce_player(int type) //pobla la cantidad de jugadores activos cuando se crean, retorna 1 si no hay espacio
+{
+    for (int i = 0; i < 5; i++){
+        if (active_players[i] != NULL){active_players[i] = type;}
+        return 0;
+    }
+    return 1;
+}
+
+bool active_class(int type) //revisa si existe un jugador de clase type activo
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (active_players[i] == type){return true;}
+    }
+    return false;
+}
+
+// Clases
+
+void estocada(Clase* attacker, Clase* enemy)
+{
+    enemy -> current_health -= 1000;
+    if (enemy->current_health <= 0)
+    {
+        printf("El jugador murió\n");
+    }
+    if (enemy -> bleeding < 3) {
+        enemy -> bleeding++;
+    }
+}
+
+void corte_cruzado(Clase* attacker, Clase* enemy)
+{
+    enemy -> current_health -= 3000;
+    if (enemy->current_health <= 0)
+    {
+        printf("El jugador murió\n");
+    }
+}
+
+void distraer(Clase* attacker, Clase* enemy)
+{
+    //el enemy en el proximo turno debe atacar al último
+    //cazador en ocupar distraer  
+    enemy -> distracted = 1;
+    enemy -> target = attacker;
+}
+
+void curar(Clase* attacker, Clase* friend)
+{
+    //no se puede tener más que la vida inicial
+    if ((friend -> current_health + 2000) >= friend->initial_health)
+    {
+        friend -> current_health = friend -> initial_health;
+    }
+    else
+    {
+        friend -> current_health += 2000;
+    }
+}
+
+void destello_regenerador(Clase* attacker, Clase** friends, int num_friends, Clase* enemy)
+{
+    int num = (rand() % (2000 - 750 + 1)) + 750; //numero random
+    int regeneracion = (int)(num/2); // redondeo hacia arriba
+    
+    int num_friend = rand() % (num_friends + 1);
+    Clase* friend = friends[num_friend];
+    //no se puede tener más que la vida inicial
+    if ((friend -> current_health + regeneracion) >= friend->initial_health)
+    {
+        friend -> current_health = friend -> initial_health;
+    }
+    else
+    {
+        friend -> current_health += regeneracion;
+    }
+
+    enemy -> current_health -= num;
+}
+
+void descarga_vital(Clase* attacker, Clase* enemy)
+{
+    int dano = 2 * (attacker -> initial_health - attacker -> current_health);
+    enemy -> current_health -= dano;
+    if (enemy->current_health <= 0)
+    {
+        printf("El jugador murió\n");
+    }
+}
+
+void inyeccion_sql(Clase* attacker, Clase* friend)
+{
+    // bool de inyeccion y numero que dure 2 turnos? para duplicar mientras bool sea true
+    friend -> duplicated_attack = 1;
+    friend -> rounds_duplicated = 0;
+}
+
+void ataque_ddos(Clase* attacker, Clase* enemy)
+{
+    enemy -> current_health -= 1500;
+    if (enemy->current_health <= 0)
+    {
+        printf("El jugador murió\n");
+    }
+}
+
+
+void fuerza_bruta(Clase* attacker, Clase* enemy)
+{
+    attacker -> fuerza_bruta ++;
+    if (attacker -> fuerza_bruta == 3)
+    {
+        enemy->current_health -= 10000;
+        if (enemy->current_health <= 0)
+        {
+            printf("El jugador murió\n");
+        }
+        attacker -> fuerza_bruta = 0;
+    }
+    
+}
+
+// Monstruos
 void ruzgar(Clase* attacker, Clase* enemy)
 {
     enemy -> current_health -= 1000;
@@ -102,5 +229,34 @@ void espina_venenosa(Clase* attacker, Clase* enemy)
 
 void copia(Clase* attacker, Clase* enemy)
 {
-    // Hay que es
+    int classes = [0, 0, 0];
+    for (int i = 0; i < 3; i++)
+    {
+        if (active_class(i)){classes[i] = 1;}
+    }
+    while (1)
+    {
+        int dice = rand() % 3;
+        if (classes[dice] == 1 && dice == 0)
+        {
+            int spell_dice = rand() % 3;
+            if (spell_dice == 0){return estocada(attacker, enemy);}
+            else if (spell_dice == 1){return corte_cruzado(attacker, enemy);}
+            else {return distraer(attacker, enemy);}//tiene que distraer a un jugador
+        }
+        else if (classes[dice] == 1 && dice == 1)
+        {
+            int spell_dice = rand() % 3;
+            if (spell_dice == 0){return curar(attacker, attacker);} //se tiene que healear a si mismo
+            else if (spell_dice == 1){return destello_regenerador(attacker, enemy);} // revisar como funciona destello_regenerador para hacer que el Ruz se healee a si mismo cuando copie esta habilidad
+            else {return descarga_vital(attacker, enemy);}
+        }
+        else if (classes[dice] == 1 && dice == 3)
+        {
+            int spell_dice = rand() % 3;
+            if (spell_dice == 0){return inyeccion_sql(attacker, attacker);}
+            else if (spell_dice == 1){return ataque_ddos(attacker, enemy);} 
+            else {return fuerza_bruta(attacker, enemy);}
+        }
+    }
 }
--- src/monster_hunter/classes.h
+++ src/monster_hunter/classes.h
@@ -5,6 +5,7 @@
 
 struct clase;
 typedef struct clase Clase;
+int active_players = [NULL, NULL, NULL, NULL, NULL];
 
 struct clase
 {
@@ -14,13 +15,35 @@ struct clase
     int* prev_actions;  // Esto creo que hay que hacerlo de un tamaño definido
     // La idea es hacer un array con las últimas habilidades utilizadas por la clase
     int last_action;
+    int bleeding; //puede ser de 0 a 3
     bool intoxicated;  // 0: no, 1: si
     int rounds_intoxicated;
+    int fuerza_bruta; //cuando llega a 3 hace daño
+    bool duplicated_attack; // 0: no, 1: si
+    int rounds_duplicated; // cuando llega a 2 duplicated_attack se vuekve a 0
+    bool distracted; // 0: no, 1: si
+    Clase* target; //indica el cazador que debe ser atacado en el próximo turno si está distraido
 };
 
 Clase* clase_init(int type);
 
+//jugadores
+void estocada(Clase* attacker, Clase* enemy);
+void corte_cruzado(Clase* attacker, Clase* enemy);
+void distraer(Clase* attacker, Clase* enemy);
+
+void curar(Clase* attacker, Clase* friend);
+void destello_regenerador(Clase* attacker, Clase* friend);
+void descarga_vital(Clase* attacker, Clase* enemy);
+
+void inyeccion_sql(Clase* attacker, Clase* enemy);
+void ataque_ddos(Clase* attacker, Clase* enemy);
+void fuerza_bruta(Clase* attacker, Clase* enemy);
+
+//Monstruos
 void ruzgar(Clase* attacker, Clase* enemy);
 void coletazo(Clase* attacker, Clase** enemies_array, int enemies_length);
 void salto(Clase* attacker, Clase* enemy);
 void espina_venenosa(Clase* attacker, Clase* enemy);
+int introduce_player(int type);
+bool active_class(int type);
